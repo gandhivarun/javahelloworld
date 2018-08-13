5ca6f3a3b3ea8bf97b214222ec0a1b2d0991364f
c-Varun Gandhi <vgandhi@numerix.com>
2018-08-13 12:49:02 +1000

    first change

--- src/main/java/com/vogella/build/maven/java/App.java
+++ src/main/java/com/vogella/build/maven/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Varun Gandhi!" );
+        System.out.println( "Hello World Varun Gandhi change1!" );
     }
 }
